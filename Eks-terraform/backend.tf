2e0b1205c660e76cabcd9d107f35cc3771df44a9
Narasimha <123463333+Narasimha76@users.noreply.github.com>
2024-01-31 15:14:14 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "narasimha-v1" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "Jenkins-eks/terraform.tfstate"
     region = "ap-south-1"
   }
 }
